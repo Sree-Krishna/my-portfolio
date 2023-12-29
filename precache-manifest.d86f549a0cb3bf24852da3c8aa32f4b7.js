df1aa05d5dfa60a3f937cb8bf25d12f6e51eedcc
Sree-Krishna <sreekrishnadav@gmail.com>
2023-12-28 19:06:34 -0500

    Updates

---
+++ precache-manifest.d86f549a0cb3bf24852da3c8aa32f4b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9bcdd3f47b38f11182bd1ebfedda59",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "89bd6ac5ebf1b10f0610",
+    "url": "/my-portfolio/static/css/main.b906d0f4.chunk.css"
+  },
+  {
+    "revision": "2194437fd98d81df00d4",
+    "url": "/my-portfolio/static/js/2.caa070c9.chunk.js"
+  },
+  {
+    "revision": "c7df833156a1790b4f9c0891a7e831c3",
+    "url": "/my-portfolio/static/js/2.caa070c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092690fe4a1b5e07a3e3",
+    "url": "/my-portfolio/static/js/3.a5d6bda9.chunk.js"
+  },
+  {
+    "revision": "89bd6ac5ebf1b10f0610",
+    "url": "/my-portfolio/static/js/main.07599dfa.chunk.js"
+  },
+  {
+    "revision": "c095245982493c479bd2",
+    "url": "/my-portfolio/static/js/runtime-main.e031bf11.js"
+  }
+]);
